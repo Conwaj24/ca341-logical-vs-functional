c190fb08757358a2907e49a194618336be72b9e3
JCoMcL <rapcyss@gmail>
2020-12-06 20:45:43 +0000

    add preorder, postorder, and inorder

--- functional/main.c
+++ functional/main.c
@@ -7,10 +7,33 @@ node *insert(int x, node *tree) {
 bool search(int x, node *tree) {
 	return searchTreeForNumber(x, tree)->data == x;
 }
-node *preorder(node *tree);
-node *inorder(node *tree);
-node *postorder(node *tree);
-
+list *preorder(node *tree) {
+	return concatList(
+		newList(tree),
+		concatList(
+			preorder(tree->left),
+			preorder(tree->right)
+		)
+	);
+}
+list *inorder(node *tree) {
+	return concatList(
+		inorder(tree->left),
+		concatList(
+			newList(tree),
+			inorder(tree->right)
+		)
+	);
+}
+list *postorder(node *tree) {
+	return concatList(
+		postorder(tree->left),
+		concatList(
+			postorder(tree->right),
+			newList(tree)
+		)
+	);
+}
 node *remove(int key, node *tree) {
 	if (!search(key, tree))
 		return tree;
